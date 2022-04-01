a81e230d6dcc40ce4bf489917a9c7072e032fbc6
José Leles <jojeca.leles@gmail.com>
2022-04-01 19:34:07 -0300

    Add files via upload

---
+++ AdvancedDatabaseEBigData/aula 1.sql
@@ -0,0 +1,23 @@
+SELECT First_name FROM employees WHERE department_id = 80;
+
+select salary
+from employees
+where employee_id = 145;
+
+-- DCL Data Control Language
+
+-- GRANT
+-- REVOKE
+
+SELECT * FROM rm84201.empvu80;
+
+CREATE SYNONYM emp
+FOR pf0645.empvu80
+
+SELECT object_type, object_name FROM user_objects ORDER BY object_type, object_name WHERE [name] = 'KAUANEH'
+
+SELECT * FROM pf0645.empvu80;
+SELECT * FROM emp1;
+CREATE VIEW KauanEh AS SELECT 'Ele eh' AS Genero FROM dual;
+
+GRANT SELECT ON KauanEh TO rm84201
